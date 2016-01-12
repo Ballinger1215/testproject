a542a517e9de6f4c1d2545d00b2617de2e22b9cd
mbolli4593 <mbolli4593@WS-AC-CTD-37001.suscorp.edu>
2016-01-11 19:41:49 -0500

    new commit

---
+++ src/appendixb_pg4/AppendixB_pg4.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appendixb_pg4;
+
+import java.util.Scanner;
+
+/**
+ * @author Mark Bollinger
+ */
+public class AppendixB_pg4 {
+        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Automobile markCar = new Automobile();
+            Automobile henryCar = new Automobile();      
+            markCar.Make = "nissan";
+            markCar.Mileage = 150000;
+            markCar.Model = "altima";
+            markCar.Year = 2005;
+            
+            henryCar.Make = "ford";
+            henryCar.Mileage = 15;
+            henryCar.Model = "escape";
+            henryCar.Year = 2010;
+            
+            markCar.increaseMileage(100);
+            henryCar.increaseMileage(211);
+            
+            //System.out.println(markCar);
+            //System.out.println(henryCar);
+            System.out.println(markCar.setMileage(1000));
+            System.out.println(henryCar.setMileage(1111));
+      
+        
+    }
+    public static class Automobile{
+       public Automobile(){
+           autoCount = autoCount + 1;
+       }
+          String type;
+          String Make;
+          String Model;
+          int Year;
+          int Mileage;
+          static int autoCount = 0;
+          
+          public void increaseMileage(int mileage){
+              Mileage = Mileage + mileage;
+          }
+          
+         public String toString(){
+            return Make + " " + Model + " " + Mileage;
+        
+        }
+         
+         public Automobile setMileage(int mileage){
+             Mileage = mileage;
+             return this;
+         }
+         
+    }
+}
